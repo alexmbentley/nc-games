3c08785204a3760c5eacbfae4a5f5969340b2fcf
alexmbentley <106654240+alexmbentley@users.noreply.github.com>
2022-09-28 18:36:13 +0100

    need to show the comment with optimistic rendering

--- src/components/CommentPoster.jsx
+++ src/components/CommentPoster.jsx
@@ -1,3 +1,77 @@
-const CommentPoster = (id) => {};
+import { useState } from 'react';
+import { getComments, postComment } from './Api';
+import CommentCard from './CommentCard';
+
+const CommentPoster = ({ reviewId, comments }) => {
+  const [user, setUser] = useState('jessjelly');
+  const [formBody, setFormBody] = useState('');
+  const [commentReturn, setCommentReturn] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [commentComplete, setCommentComplete] = useState('Add a comment...');
+  const textDisabled = formBody.length === 0;
+
+  let m = new Date();
+  const dateString =
+    m.getUTCFullYear() +
+    '-' +
+    (m.getUTCMonth() + 1) +
+    '-' +
+    m.getUTCDate() +
+    ' ' +
+    m.getUTCHours() +
+    ':' +
+    m.getUTCMinutes() +
+    ':' +
+    m.getUTCSeconds();
+
+  const handleSubmit = (e, props) => {
+    console.log(comments, '<<< set comments');
+    e.preventDefault();
+    let commentObj = { username: user, body: formBody };
+    let testObj = {
+      author: user,
+      body: formBody,
+      comment_id: 9999999,
+      created_at: m,
+      review_id: 999999,
+      votes: 0,
+    };
+    console.log(commentObj, '<< comment obj');
+    postComment(reviewId, commentObj)
+      .then(({ data }) => {
+        console.log(data, '<< returned data');
+        setCommentReturn(data);
+        setIsLoading(false);
+        console.log(comments, '<<< mutated comments');
+        setCommentComplete('Comment sent!');
+        setFormBody('');
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        setIsError(true);
+        setCommentComplete(false);
+      });
+  };
+  return (
+    <div className="commentPoster">
+      <form onSubmit={handleSubmit}>
+        <label>
+          {commentComplete}
+          <input
+            value={formBody}
+            onChange={(event) => setFormBody(event.target.value)}
+            className="commentBox"
+            type="text"
+            name="comment"
+          />
+        </label>
+        <button disabled={textDisabled} type="sumbit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default CommentPoster;
--- src/components/SingleReview.jsx
+++ src/components/SingleReview.jsx
@@ -11,7 +11,6 @@ const SingleReview = () => {
   const [comments, setComments] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
-  const [comError, setComError] = useState(false);
   const { id } = useParams();
 
   useEffect(() => {
@@ -27,29 +26,35 @@ const SingleReview = () => {
   }, [id]);
 
   useEffect(() => {
-    getComments(id)
-      .then(({ data }) => {
-        setComments(data);
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        setIsLoading(false);
-        setComError(true);
-      });
+    getComments(id).then(({ data }) => {
+      setComments(data);
+      console.log(comments, '<<< comments');
+      setIsLoading(false);
+    });
   }, [id]);
 
   if (Object.hasOwn(singleReview, 'review_id'))
     return (
       <div>
         <h2 className="pageTitle">Review</h2>
-        <CommentPoster
-          key={singleReview.review_id}
-          reviewId={singleReview.review_id}
-        />
         <SingleReviewCard key={singleReview.review_id} review={singleReview} />
-        {comments.map((comment) => (
-          <CommentCard key={comment.comment_id} comment={comment} />
-        ))}
+        <CommentPoster
+          key={singleReview.votes}
+          reviewId={singleReview.review_id}
+          comments={comments}
+        />
+        {Array.isArray(comments) ? (
+          <div>
+            {console.log(comments, '<<< inside comments')}
+            {comments.map((comment) => (
+              <CommentCard key={comment.comment_id} comment={comment} />
+            ))}
+          </div>
+        ) : (
+          <>
+            <p className="commentCard">No Comments for this post</p>
+          </>
+        )}
       </div>
     );
 
@@ -59,13 +64,6 @@ const SingleReview = () => {
     return (
       <div>
         <h2 className="pageTitle">ID does not exist</h2>
-      </div>
-    );
-
-  if (comError)
-    return (
-      <div>
-        <h2 className="pageTitle">No comments for this post</h2>
       </div>
     );
 };
