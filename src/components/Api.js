d146fbcf2c69edfb0486a9443aef37ccb0cbe82e
alexmbentley <106654240+alexmbentley@users.noreply.github.com>
2022-11-28 18:17:27 +0000

    updated api calls to cyclic

--- src/components/Api.js
+++ src/components/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const getReviews = (param, sortBy, orderBy) => {
   return axios
-    .get(`https://alexs-backend-project.herokuapp.com/api/reviews/`, {
+    .get(`https://long-ruby-goat.cyclic.app/api/reviews/`, {
       params: { category: param.category, sort_by: sortBy, order: orderBy },
     })
     .then((response) => {
@@ -11,34 +11,30 @@ export const getReviews = (param, sortBy, orderBy) => {
 };
 
 export const getCategories = () => {
-  return axios.get(
-    `https://alexs-backend-project.herokuapp.com/api/categories`
-  );
+  return axios.get(`https://long-ruby-goat.cyclic.app/api/categories`);
 };
 
 export const getSingleReview = (id) => {
-  return axios.get(
-    `https://alexs-backend-project.herokuapp.com/api/reviews/${id}`
-  );
+  return axios.get(`https://long-ruby-goat.cyclic.app/api/reviews/${id}`);
 };
 
 export const addVote = (id, votesObj) => {
   return axios.patch(
-    `https://alexs-backend-project.herokuapp.com/api/reviews/${id}`,
+    `https://long-ruby-goat.cyclic.app/api/reviews/${id}`,
     votesObj
   );
 };
 
 export const getComments = (id) => {
   return axios.get(
-    `https://alexs-backend-project.herokuapp.com/api/reviews/${id}/comments`
+    `https://long-ruby-goat.cyclic.app/api/reviews/${id}/comments`
   );
 };
 
 export const postComment = (id, commentObj) => {
   return axios
     .post(
-      `https://alexs-backend-project.herokuapp.com/api/reviews/${id}/comments`,
+      `https://long-ruby-goat.cyclic.app/api/reviews/${id}/comments`,
       commentObj
     )
     .then((response) => {
@@ -47,7 +43,5 @@ export const postComment = (id, commentObj) => {
 };
 
 export const deleteComment = (id) => {
-  return axios.delete(
-    `https://alexs-backend-project.herokuapp.com/api/comments/${id}`
-  );
+  return axios.delete(`https://long-ruby-goat.cyclic.app/api/comments/${id}`);
 };
